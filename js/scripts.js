7fd129e5b58b1c26f61e68617dab29fbfb24f89e
acenationfn <poop66953@gmail.com>
2023-06-05 20:01:02 -0700

    update
    -loading screen
    -description etc.

--- js/scripts.js
+++ js/scripts.js
@@ -97,3 +97,137 @@ function goto( link ){
 //             });
 //     }
 // });
+
+// =============================
+// loading animation
+// =============================
+
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2"),
+    maskBG: document.getElementsByClassName("maskbg")[0]
+};
+
+let endLoading = false;
+
+
+const texts = [
+    "ACE",
+    "NATION"
+];
+
+let totalTime = Math.random() * 8 + 3;
+const morphTime = totalTime / 2;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+
+    let fraction = morph / morphTime;
+
+    if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+
+    setMorph(fraction);
+}
+
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    //console.log("textIdx:" + textIndex);
+    if (textIndex < texts.length * 2 - 1) {
+        requestAnimationFrame(animate);
+    }
+
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+
+    cooldown -= dt;
+
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+            textIndex++;
+        }
+        doMorph();
+    } else {
+        doCooldown();
+    }
+    if (endLoading == true) {            
+        elts.maskBG.classList.add('maskbg-transition');
+        elts.maskBG.classList.add('maskbg-hidden');
+    }
+
+    if (textIndex < texts.length * 2 - 1) {
+    } else {
+        endLoading = true;
+    }
+}
+
+animate();
+
+elts.maskBG.addEventListener('transitionend', function() {
+    elts.maskBG.classList.remove('box-transition');
+}, false);
+
+
+//========  Scroll Lock ===========//
+function disableScroll() {
+    // Get the current page scroll position in the vertical direction
+    scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+    // Get the current page scroll position in the horizontal direction 
+    scrollLeft =
+        window.pageXOffset || document.documentElement.scrollLeft;
+    // if any scroll is attempted,
+    // set this to the previous value
+    window.onscroll = function () {
+        console.log("score to : top:" + scrollTop);
+        window.scrollTo({
+            top: screenTop,
+            left: screenLeft,
+            behavior: "instant",
+        });
+    };
+}
+
+function enableScroll() {
+    window.onscroll = function () { };
+}
+disableScroll();
+elts.maskBG.addEventListener('transitionend', function () {
+    elts.maskBG.classList.remove('maskbg-transition');
+    enableScroll();
+}, false);
